381d94b7298108464bb7bef3b5715355b106905a
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2026-06-13 20:51:41 +0100

    Remove redundant __dcc_write function.
    This function was not called anywhere.
    
    Removing it is not a breaking change because it begins with "__", which
    means it was for internal use only.

--- arm-dcc/src/lib.rs
+++ arm-dcc/src/lib.rs
@@ -255,18 +255,3 @@ pub fn write_fmt(args: fmt::Arguments) {
 pub fn write_str(string: &str) {
     write_all(string.as_bytes())
 }
-
-#[cfg(target_arch = "arm")]
-core::arch::global_asm!(
-    r#"
-    // Routine for putting data in the DCC register
-    .section .text.__dcc_write
-    .global __dcc_write
-__dcc_write:
-1:  mrc     p14, 0, r1, c0, c1, 0
-    tst     r1, #536870912      /* 0x20000000 */
-    bne     1b
-    mcr     p14, 0, r0, c0, c5, 0
-    bx      lr
-    "#
-);
